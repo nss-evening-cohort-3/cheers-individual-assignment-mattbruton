460ac42a00a56fb90443d7f94269a84ba1947c95
MATT BRUTON <the.bruton@gmail.com>
2016-07-10 11:10:24 -0500

    Accepting a user's name and printing basic cheer. Need to check for vowels when cheering

--- Program.cs
+++ Program.cs
@@ -9,6 +9,25 @@ namespace Cheers
     {
         public static void Main(string[] args)
         {
+            Console.WriteLine("What is your name?");
+
+            // Prompt user for their name.
+            string userName = Console.ReadLine();
+            string userNameToLowercase = userName.ToLower();
+            string userNameToUppercase = userName.ToUpper();
+
+            // Greet user before the cheer.
+            Console.WriteLine("Get ready for your cheer, " + userName + "!");
+
+
+            // Loop through each character in userName and print to console along with cheer message.
+            foreach (char letter in userNameToLowercase)
+            {
+                Console.WriteLine("Give me a " + letter + "...");
+            }
+
+            // Display userName and tell them they are wonderful.
+            Console.WriteLine(userNameToUppercase + "... is GRAND!");
         }
     }
 }
