5e52e34ddc66f23aad3620540d03f56f538c5d93
MATT BRUTON <the.bruton@gmail.com>
2016-07-10 12:37:36 -0500

    Completed task to check if letter is a vowel when looping through letters in user's name

--- Program.cs
+++ Program.cs
@@ -13,18 +13,25 @@ namespace Cheers
 
             // Prompt user for their name.
             string userName = Console.ReadLine();
-
+            string[] vowels = { "a", "e", "i", "o", "u" };
+            
             // Greet user before the cheer.
             Console.WriteLine("Get ready for your cheer, " + userName + "!");
 
-
             // Loop through each character in userName and print to console along with cheer message.
-            
             foreach (char letter in userName)
             {   
                 // Make each letter in userName lower case for cheer.
                 string letterToLower = letter.ToString().ToLower();
-                Console.WriteLine("Give me a " + letterToLower + "...");
+                // Check if each letter is a vowel so we can write proper English.
+                if (vowels.Contains(letterToLower))
+                    {
+                        Console.WriteLine("Give me an " + letterToLower + "...");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Give me a " + letterToLower + "...");
+                    }
             }
 
             // Display userName in upper case and tell them they are wonderful.
