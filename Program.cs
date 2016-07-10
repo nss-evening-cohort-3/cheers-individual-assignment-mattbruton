2c72ae08ef6fb50c019d1e16597f5d93f8e4df84
MATT BRUTON <the.bruton@gmail.com>
2016-07-10 12:18:56 -0500

    refactored code for manipulating userName in cheer

--- Program.cs
+++ Program.cs
@@ -13,21 +13,22 @@ namespace Cheers
 
             // Prompt user for their name.
             string userName = Console.ReadLine();
-            string userNameToLowercase = userName.ToLower();
-            string userNameToUppercase = userName.ToUpper();
 
             // Greet user before the cheer.
             Console.WriteLine("Get ready for your cheer, " + userName + "!");
 
 
             // Loop through each character in userName and print to console along with cheer message.
-            foreach (char letter in userNameToLowercase)
-            {
-                Console.WriteLine("Give me a " + letter + "...");
+            
+            foreach (char letter in userName)
+            {   
+                // Make each letter in userName lower case for cheer.
+                string letterToLower = letter.ToString().ToLower();
+                Console.WriteLine("Give me a " + letterToLower + "...");
             }
 
-            // Display userName and tell them they are wonderful.
-            Console.WriteLine(userNameToUppercase + "... is GRAND!");
+            // Display userName in upper case and tell them they are wonderful.
+            Console.WriteLine(userName.ToUpper() + "... is GRAND!");
         }
     }
 }
