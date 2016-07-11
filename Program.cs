c538be2898ee1c677ec0bc81abc5a11dc9638fb6
MATT BRUTON <the.bruton@gmail.com>
2016-07-11 18:40:38 -0500

    Added line asking user their name in class

--- Program.cs
+++ Program.cs
@@ -9,7 +9,9 @@ namespace Cheers
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            Console.WriteLine("What's your name?");
+
+            string userName = Console.ReadLine();
         }
     }
 }
