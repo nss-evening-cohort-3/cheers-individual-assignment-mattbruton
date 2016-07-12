aee38fe1d08ed4379f67659ffc9c50a3380d1f59
MATT BRUTON <the.bruton@gmail.com>
2016-07-11 20:11:41 -0500

    added logic for looping through username to complete cheer

--- Program.cs
+++ Program.cs
@@ -10,13 +10,26 @@ namespace Cheers
         public static void Main(string[] args)
         {   
             Console.WriteLine("What's your name?");
-
             string prompt = "> ";
-
             Console.Write(prompt);
-
             string userName = Console.ReadLine();
 
+            string[] vowels = {"a","e","i","o","u"};
+
+            foreach (char letter in userName)
+            {
+                string letterToLower = letter.ToString().ToLower();
+                string properArticle;
+                if (vowels.Contains(letterToLower))
+                {
+                    properArticle = "an";
+                }
+                else
+                {
+                    properArticle = "a";
+                }
+                Console.WriteLine("Give me " + properArticle + " " + letterToLower);
+            }
             Console.WriteLine(userName.ToUpper() + " is.. GRAND!");
         }
     }
